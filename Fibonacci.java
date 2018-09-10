83231b54c1fcae5fda57ea299c33e0bf6d90812e
oscarcamilopulidop <38712273+oscarcamilopulidop@users.noreply.github.com>
2018-09-09 20:36:22 -0500

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,63 @@
+package fibonacci;
+
+import java.util.*;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+//SHORT
+       /*short a = 1;
+        short b = 2;
+        System.out.println("Ingrese n: ");
+        short n = sc.nextShort();
+        for (short i = 4; i <= n; i++) {
+            b += a;
+            a = (short) (b - a);
+        }
+        if (n == 0) {
+            System.out.println(0 + " ");
+        } else if (n == 1 || n == 2) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.println(b + " ");
+        }*/
+        
+//INT        
+/*        int a = 1;
+        int b = 2;
+        System.out.println("Ingrese n: ");
+        int n = sc.nextInt();
+        for (int i = 4; i <= n; i++) {
+            b += a;
+            a = b - a;
+        }
+        if (n == 0) {
+            System.out.println(0 + " ");
+        } else if (n == 1 || n == 2) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.println(b + " ");
+        }*/
+//LONG
+/*
+        long a = 1;
+        long b = 2;
+        System.out.println("Ingrese n: ");
+        long n = sc.nextInt();
+        for (long i = 4; i <= n; i++) {
+            b += a;
+            a = b - a;
+        }
+        if (n == 0) {
+            System.out.println(0 + " ");
+        } else if (n == 1 || n == 2) {
+            System.out.print(1 + " ");
+        } else {
+            System.out.println(b + " ");
+        }
+        System.out.println(Long.MAX_VALUE);*/
+    }
+
+}
